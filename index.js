4e0076b2c45e6095da20cebd70faf918825d82eb
John David Gonzalez <jhndagon@gmail.com>
2020-04-19 20:22:07 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function determinarTriangulo(){
+    var ladoA = document.getElementById("ladoA").value;
+    var ladoB = document.getElementById("ladoB").value;
+    var ladoC = document.getElementById("ladoC").value;
+    if(ladoA < 0 || ladoB < 0 || ladoC < 0){
+        document.getElementById("resultado").innerHTML="Ingrese valores positivos.";
+    }
+    if(ladoA == ladoB || ladoB == ladoC || ladoA == ladoC){
+        if(ladoA == ladoB && ladoA== ladoC){
+            document.getElementById("resultado").innerHTML="Es un triángulo equilatero.";
+        }
+        else {
+            document.getElementById("resultado").innerHTML="Es un triángulo isosceles.";
+        }
+    }
+    else {
+        document.getElementById("resultado").innerHTML="Es un triángulo escaleno.";
+    }
+}
+
+document.location.href = "/?#"
