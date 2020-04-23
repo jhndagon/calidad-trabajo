7354023cd59481b95daca73df16bee6a77e233ee
John David Gonzalez <jhndagon@gmail.com>
2020-04-23 18:12:15 -0500

    Fix: al presionar boton sin datos mostraba triangulo escaleno

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ function determinarTriangulo(){
     var ladoA = document.getElementById("ladoA").value;
     var ladoB = document.getElementById("ladoB").value;
     var ladoC = document.getElementById("ladoC").value;
-    if(ladoA < 0 || ladoB < 0 || ladoC < 0){
-        document.getElementById("resultado").innerHTML="Ingrese valores positivos.";
-    }
-    if(ladoA == ladoB || ladoB == ladoC || ladoA == ladoC){
-        if(ladoA == ladoB && ladoA== ladoC){
-            document.getElementById("resultado").innerHTML="Es un triángulo equilatero.";
+    if(ladoA || ladoB || ladoC){
+        if(ladoA < 0 || ladoB < 0 || ladoC < 0){
+            document.getElementById("resultado").innerHTML="Ingrese valores positivos.";
+        }
+        if(ladoA == ladoB || ladoB == ladoC || ladoA == ladoC){
+            if(ladoA == ladoB && ladoA== ladoC){
+                document.getElementById("resultado").innerHTML="Es un triángulo equilatero.";
+            }
+            else {
+                document.getElementById("resultado").innerHTML="Es un triángulo isosceles.";
+            }
         }
         else {
-            document.getElementById("resultado").innerHTML="Es un triángulo isosceles.";
+            document.getElementById("resultado").innerHTML="Es un triángulo escaleno.";
         }
-    }
-    else {
-        document.getElementById("resultado").innerHTML="Es un triángulo escaleno.";
     }
 }
 
